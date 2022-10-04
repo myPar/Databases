f0af41e6764f8f268f2e924d8f5ee4842c3b55c9
Andrew <andrebobrovs@mail.ru>
2022-10-04 11:45:31 +0700

    implement query which selects teacher spec info

---
+++ university_database/queries.sql
@@ -0,0 +1,18 @@
+SELECT teacher_id, teacher_name, teacher_surname, scientific_title,
+       subject_name, subject_type, timetable_subject_id
+FROM
+((
+    (
+    (TeachersSpecialization INNER JOIN
+        (SELECT Teachers.id, Teachers.teacher_name, Teachers.teacher_surname, Teachers.scientific_title FROM Teachers WHERE Teachers.id = [enter the teacher id:]) AS teachers_f
+    ON TeachersSpecialization.teacher_id = teachers_f.id)
+
+    INNER JOIN TimetableSubjects
+
+    ON TimetableSubjects.id = TeachersSpecialization.timetable_subject_id
+    )
+    INNER JOIN (SELECT Subjects.id, Subjects.subject_name FROM Subjects) AS subjects_f
+    ON subjects_f.id = TimetableSubjects.subject_id
+)
+INNER JOIN SubjectTypes 
+ON SubjectTypes.id = TimetableSubjects.subject_type_id);
