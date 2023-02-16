fff3d6e29fad3065178011a64e5c7ae529165ab2
myPar <andrebobrovs@mail.ru>
2023-02-16 21:25:37 +0700

    added triggers task

--- triggers.sql
+++ triggers.sql
@@ -22,3 +22,23 @@ FOR EACH ROW
 BEGIN
     SELECT students_id_seq.NEXTVAL INTO :NEW.id FROM dual;
 END;
+
+CREATE OR REPLACE
+TRIGGER check_insert_students
+BEFORE INSERT ON student
+FOR EACH ROW
+BEGIN
+    IF EXISTS (SELECT * FROM teacher WHERE :NEW.name = teacher.name 
+    AND :NEW.last_name = teacher.last_name)
+    THEN
+        RAISE_APPLICATION_ERROR(-1, 'student can not be a teacher');
+    END IF;
+END;
+
+CREATE OR REPLACE
+TRIGGER cascade_delete_teacher
+BEFORE DELETE ON teacher
+FOR EACH ROW
+BEGIN
+    delete from st_teac WHERE st_teac.id_teac = :OLD.id;
+END;
